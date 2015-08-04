d5b390f7dd854c7c786165a5aa8ce51a3b147cf7
Daniel Corson <danl@alum.mit.edu>
2015-08-04 12:12:38 -0400

    wmv for multiple screens

--- hs/wmv/src/Wmv.hs
+++ hs/wmv/src/Wmv.hs
@@ -3,6 +3,7 @@
 import Control.Applicative
 import Control.Arrow
 import Control.Concurrent
+import Control.Monad
 import Data.List
 import Data.List.Utils
 import Data.Ratio
@@ -21,22 +22,21 @@ data Wmv = Wmv
     , wX :: !(P2 Rational)
     , wY :: !(P2 Rational)
     , wTitle :: !String
+    , wVerbose :: !Bool
     }
     deriving Show
 
-charW, charH :: Rational
-charW = 8
-charH = 13
+charWAndH :: P2 Rational
+charWAndH = P2 8 13
 
-s1UnitChW, s1UnitChH :: Rational
-s1UnitChW = 80
-s1UnitChH = 40
+scrsUnitChWAndH :: [P2 Rational]
+scrsUnitChWAndH = [P2 80 40, P2 80 40]
 
 pInit :: P2 Rational
 pInit = P2 0 1
 
 wInit :: Wmv
-wInit = Wmv 1 pInit pInit ""
+wInit = Wmv 1 pInit pInit "" False
 
 readCoord :: Rational -> ReadS (P2 Rational)
 readCoord _ arg@('+':y)
@@ -59,20 +59,26 @@ readRat ('.':y)
   | otherwise = []
 readRat x = readFloat x
 
-procArgs :: Rational -> Rational -> [String] -> Wmv
-procArgs s1WUnits s1HUnits = procScr wInit
+procArgs :: [P2 Rational] -> [String] -> Wmv
+procArgs scrsWAndHUnits = procVerbose wInit
   where
+    procVerbose w ("-v":rest) = procScr (w {wVerbose = True}) rest
+    procVerbose w rest = procScr w rest
+
     procScr w (('s':nStr):rest) = procX (w {wScr = read nStr}) rest
     procScr w rest = procX w rest
 
     procX w (arg:rest) =
         procY (w {wX = coord1}) rest
       where
-        [(coord1, "")] = readCoord s1WUnits arg
+        coord1 = case readCoord (p1 $ scrsWAndHUnits !! (wScr w - 1)) arg of
+          [(c, "")] -> c
+          hm -> error $
+            "arg: " ++ show arg ++ "; readCoord s1WUnits arg: " ++ show hm
 
     procY w args
       | arg:restArgs <- args,
-        [(coord2, "")] <- readCoord s1HUnits arg =
+        [(coord2, "")] <- readCoord (p2 $ scrsWAndHUnits !! (wScr w - 1)) arg =
         procTitle (w {wY = coord2}) restArgs
       | otherwise = procTitle w args
 
@@ -80,17 +86,23 @@ procArgs s1WUnits s1HUnits = procScr wInit
     procTitle w [arg] = w {wTitle = arg}
     procTitle _ args = error $ "Multiple arguments for title: " ++ show args
 
-doWmv :: Rational -> Rational -> Wmv -> IO ()
-doWmv s1UnitW s1UnitH w =
-    let [gX, gY] = map (show . floor)
-            [s1UnitW * p1 (wX w), s1UnitH * p1 (wY w)]
+doWmv :: [P2 Rational] -> [P2 Rational] -> Wmv -> IO ()
+doWmv scrsPixelWAndH scrsUnitWAndH w = do
+    let scrUnitWAndH = scrsUnitWAndH !! (wScr w - 1)
+        gX = show . floor $ sum (map p1 $ take (wScr w - 1) scrsPixelWAndH) +
+            p1 scrUnitWAndH * p1 (wX w)
+        gY = show . floor $ p2 scrUnitWAndH * p1 (wY w)
         [gW, gH] = map (show . floor)
-            [s1UnitW * p2 (wX w), s1UnitH * p2 (wY w)]
+            [p1 scrUnitWAndH * p2 (wX w), p2 scrUnitWAndH * p2 (wY w)]
         [tW, tH] = map (show . floor)
-            [s1UnitW * p2 (wX w) / charW, s1UnitH * p2 (wY w) / charH]
-        doMv = rawSystem "wmctrl"
-            ["-r", wTitle w, "-e", intercalate "," ["0", gX, gY, gW, gH]]
-    in case wTitle w of
+            [ p1 scrUnitWAndH * p2 (wX w) / p1 charWAndH
+            , p2 scrUnitWAndH * p2 (wY w) / p2 charWAndH
+            ]
+        cmd = "wmctrl"
+        args = ["-r", wTitle w, "-e", intercalate "," ["0", gX, gY, gW, gH]]
+        doMv = rawSystem "wmctrl" args
+    when (wVerbose w) . putStrLn $ intercalate " " (cmd:args)
+    case wTitle w of
       "--xywh" -> putStrLn $ intercalate " " [gX, gY, gW, gH]
       "--terminal-xywh" -> putStrLn $ intercalate " " [gX, gY, tW, tH]
       "--geometry" -> putStrLn $ concat [gW, "x", gH, "+", gX, "+", gY]
@@ -105,16 +117,30 @@ doWmv s1UnitW s1UnitH w =
         doMv
         return ()
 
+twoWords :: String -> (String, String)
+twoWords = (\(x:y:_) -> (x, y)) . words
+
+readI :: String -> Int
+readI = read
+
 main :: IO ()
 main = do
-    s1PixelWStr:s1PixelHStr:_ <- words . replace "x" " " .  head . words .
-        head . filter ("*" `isInfixOf`) . lines <$> readProcess "xrandr" [] ""
-    let s1PixelW = fromIntegral (read s1PixelWStr :: Int)
-        s1PixelH = fromIntegral (read s1PixelHStr :: Int)
-        -- A unit is a place on the screen for a normal size text terminal.
-        s1UnitW = charW * s1UnitChW
-        s1UnitH = s1PixelH
-        s1WUnits = s1PixelW / s1UnitW
-        s1HUnits = s1PixelH / s1UnitH
+    scrsPixelWAndH <- map (
+        (\(x, y) -> P2 (fromIntegral $ readI x) (fromIntegral $ readI y)) .
+        twoWords . replace "x" " " . head . words) .
+        filter ("*" `isInfixOf`) . lines <$> readProcess "xrandr" [] ""
+    -- A unit is a place on the screen for a normal size text terminal.
+    let scrsUnitWAndH = zipWith
+            (\(P2 _scrPixelW scrPixelH) (P2 scrUnitChW _scrUnitChH) ->
+                P2 (p1 charWAndH * scrUnitChW) scrPixelH
+                )
+            scrsPixelWAndH
+            scrsUnitChWAndH
+        scrsWAndHUnits = zipWith
+            (\(P2 scrPixelW scrPixelH) (P2 scrUnitW scrUnitH) ->
+                P2 (scrPixelW / scrUnitW) (scrPixelH / scrUnitH)
+                )
+            scrsPixelWAndH
+            scrsUnitWAndH
     args <- getArgs
-    doWmv s1UnitW s1UnitH $ procArgs s1WUnits s1HUnits args
+    doWmv scrsPixelWAndH scrsUnitWAndH $ procArgs scrsWAndHUnits args
