f944d91eb3673efd46f5da539bf7b65b5050731b
Daniel P Corson <danl@alum.mit.edu>
2023-09-09 12:20:25 -1000

    cp

--- c/focusWeb.c
+++ c/focusWeb.c
@@ -8,26 +8,21 @@ inline void bufReadWord(char *wd, int &wdI) {wdI = 0; char c;
   wd[wdI++] = 0;}
 inline void bufSkipSpaces() {while (zre(buf[bufI]) == ' ') bufI++;}
 inline void bufSkipWord() {while (zre(buf[bufI++]) != ' ') {}}
-int main(int argc, char *argv[]) {
-  zre(argc == 2);
-  if (!argv[1][1] && argv[1][0] >= 'f' && argv[1][0] <= 'h') {}
-    else zre(0);
-  int w, h, maxArea = 0, wdI;
+int main(int argc, char *argv[]) {char mode;
+  if (argc == 2 && !argv[1][1] && (mode = argv[1][0]) >= 'f' && mode <= 'h')
+    {} else zre(0);
+  int w, h, value = 0, wdI;
   char wd[65536], id[65536], bestId[65536], *buf2;
   FILE* p = zre(popen("wmctrl -lG", "r"));
-  while (fgets(buf, sizeof buf, p)) {
-    bufI = 0;
-    bufReadWord(id, wdI);
-    bufSkipSpaces(); bufSkipWord();
-    bufSkipSpaces(); bufSkipWord();
-    bufSkipSpaces(); bufSkipWord();
-    bufSkipSpaces(); bufReadWord(wd, wdI); w = atoi(wd);
-    bufSkipSpaces(); bufReadWord(wd, wdI); h = atoi(wd);
-    bufSkipSpaces(); bufSkipWord();
-    buf2 = buf + bufI;
-    bufSkipSpaces();
+  while (fgets(buf, sizeof buf, p)) {bufI = 0; bufReadWord(id, wdI);
+    bufSkipSpaces(); bufSkipWord(); bufSkipSpaces(); bufSkipWord();
+    bufSkipSpaces(); bufSkipWord(); bufSkipSpaces();
+    bufReadWord(wd, wdI); w = atoi(wd); bufSkipSpaces();
+    bufReadWord(wd, wdI); h = atoi(wd); bufSkipSpaces(); bufSkipWord();
+    bufSkipSpaces(); buf2 = buf + bufI;
     if (strcmp(" - Google Chrome\n", buf2 + strlen(buf2) - 17)) continue;
-    if (w * h <= maxArea) continue;
-    maxArea = w * h;
+    if (mode == 'g') 
+    if (w * h <= value) continue;
+    value = w * h;
     strcpy(bestId, id);}
   return execl("/bin/wmctrl", "wmctrl", "-ia", bestId, NULL);}
