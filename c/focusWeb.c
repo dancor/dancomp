35ecfb86c3faad10d0ad85e555e750c5aa500342
Daniel P Corson <danl@alum.mit.edu>
2023-09-09 18:34:29 -1000

    cp

--- c/focusWeb.c
+++ c/focusWeb.c
@@ -9,8 +9,7 @@ inline void bufReadWord(char *wd, int &wdI) {wdI = 0; char c;
 inline void bufSkipSpaces() {while (zre(buf[bufI]) == ' ') bufI++;}
 inline void bufSkipWord() {while (zre(buf[bufI++]) != ' ') {}}
 inline void endOn(char *id) {
-  printf("id:%s\n", id); exit(0);}
-  //nge(execl("/bin/wmctrl", "wmctrl", "-ia", id, NULL));}
+  nge(execl("/bin/wmctrl", "wmctrl", "-ia", id, NULL));}
 int main(int argc, char *argv[]) {char mode;
   if (argc == 2 && !argv[1][1] && (mode = argv[1][0]) >= 'f' && mode <= 'h')
     {} else zre(0);
@@ -21,8 +20,7 @@ int main(int argc, char *argv[]) {char mode;
     bufSkipSpaces(); bufSkipWord(); bufSkipSpaces(); bufReadWord(wd, wdI);
     x = atoi(wd); bufSkipSpaces(); bufReadWord(wd, wdI); y = atoi(wd);
     bufSkipSpaces(); bufReadWord(wd, wdI); w = atoi(wd); bufSkipSpaces();
-    bufReadWord(wd, wdI); h = atoi(wd); /*bufSkipSpaces(); bufSkipWord();
-    bufSkipSpaces();*/ buf2 = buf + bufI;
+    bufReadWord(wd, wdI); h = atoi(wd); buf2 = buf + bufI;
     if (strcmp(" - Google Chrome\n", buf2 + strlen(buf2) - 17)) continue;
     printf("mode:%c id:%s x:%d y:%d buf2:%s", mode, id, x, y, buf2);
     switch (mode) {case 'g': a = w * h; if (a <= val) continue;
